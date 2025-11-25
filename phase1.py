09192b1851c09badc4163c7b74b13449908f1027
Akshay <akshay.patil@wisdomleaf.com>
2025-11-25 23:33:39 +0530

    Added problem best time to buys and sell a stock

--- phase1.py
+++ phase1.py
@@ -30,5 +30,29 @@ def removeValue(val = 3):
 
 removeValue(4);
 
+#reverse a string array
+def reverseString(str = "Hello"):
+    a = list(str); # convert a string to array
+    n = len(a);
+    for i in range(n//2):
+        a[i], a[n-1-i] = a[n-1-i], a[i]; # swap 2 value in array a[i], a[n-1-i]
+    print("".join(a)); #convert array to string
+    return "".join(a);
 
+reverseString("Hello");
 
+#Best time to buy and sell a stock
+price = [7, 1, 5, 3, 6, 4, 8, 10, 9];
+# price = [ 5, 4, 2, 1];
+
+def findMaxProfit():
+    min = price[0];
+    maxProfit = 0;
+    for i in range(len(price)):
+        if maxProfit < (price[i] - min):
+            maxProfit = price[i] - min;
+        if min > price[i]:
+            min = price[i]
+    print(maxProfit);
+
+findMaxProfit()
